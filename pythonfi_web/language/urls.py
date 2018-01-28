4b1c6f976d36cc35333624a5e7f26f9cc8187cff
Edwin Mendez <emendez@gravitalsoftware.com>
2018-01-28 16:59:18 -0500

    ajax request

--- pythonfi_web/language/urls.py
+++ pythonfi_web/language/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from pythonfi_web.language.eg.views import ajax_crud, ajax_crud_create, ajax_crud_update, ajax_crud_delete
-
+from pythonfi_web.language.eg.ajax_request.views import SignUpView, validate_username
 
 urlpatterns = [
 
@@ -23,6 +23,17 @@ urlpatterns = [
         regex=r'^ajax_crud/(?P<pk>\d+)/delete$',
         view=ajax_crud_delete,
         name='ajax_crud_delete'
+    ),
+
+    url(
+        regex=r'^ajax_request',
+        view=SignUpView.as_view(),
+        name='SignUpView'
+    ),
+    url(
+        regex=r'^validate_username/',
+        view=validate_username,
+        name='validate_username'
     ),
 
 ]
